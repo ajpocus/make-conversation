752c27be150d46da92946ea498ff2f59718b20c6
Austin Pocus <ajpocus@gmail.com>
2022-01-10 18:02:30 -0500

    DialogueOption component, still needs work

--- components/DialogueOption.tsx
+++ components/DialogueOption.tsx
@@ -4,21 +4,21 @@ import useDialogue from "~/hooks/useDialogue";
 import styles from "~/styles/DialogueOption.module.css";
 
 const DialogueOption = ({ option, path }) => {
-  const { activeNPC } = useDialogue();
+  const { activeOption, setActiveOption } = useDialogue();
 
-  const topClass = isActiveOption(path) ? styles.activeOption : styles.dialogueOption;
+  const topClass = option.id === activeOption ? styles.activeOption : styles.dialogueOption;
 
   const clickHandler = useCallback(() => {
-    addOption({ })
-  }, []);
+    setActiveOption(option.id);
+  }, [setActiveOption, option]);
 
   return (
     <div className={topClass}>
-      <h3 className={styles.optionName} onClick={clickHandler}>{optionName}</h3>
+      <h3 className={styles.optionName} onClick={clickHandler}>{option.id}</h3>
 
       <div className={styles.subOptions}>
         {option?.options && (
-          Object.entries(option.options).map(([subOptionName, subOption]) => (
+          Object.entries(option.responses).map(([subOptionName, subOption]) => (
             <DialogueOption
               key={subOptionName}
               path={[...path, "options", subOptionName]}
