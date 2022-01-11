fedfac04eb846e89c0fc0213097bbe0c72afcbab
Austin Pocus <ajpocus@gmail.com>
2022-01-10 22:56:06 -0500

    some basic column setup

--- components/DialogueTree.tsx
+++ components/DialogueTree.tsx
@@ -4,30 +4,39 @@ import useDialogue from "~/hooks/useDialogue";
 import styles from "~/styles/DialogueTree.module.css";
 
 const DialogueTree = () => {
-  const { NPCs, setNPCs, activeNPC, options } = useDialogue();
+  const { NPCs, setNPCs, activeNPC, activeOption, options } = useDialogue();
 
   const npc = NPCs[activeNPC]
   const optionList = npc.options?.map((optionID) => options[optionID]);
+  const subOptions = activeOption && options[activeOption]?.responses?.map(id => options[id]);
 
   return (
     <div className={styles.dialogueTree}>
-      <h2 className={styles.activeNPC}>
-        {npc.name}
-      </h2>
+      <div className={styles.mainOptions}>
+        <h2 className={styles.activeNPC}>
+          {npc.name}
+        </h2>
 
-      {activeNPC && (
-        <p>
-          Add option to {npc.name}
-        </p>
-      )}
-      <AddOptionForm />
+        {activeNPC && (
+          <p>
+            Add option to {npc.name}
+          </p>
+        )}
+        <AddOptionForm />
 
-      {optionList && optionList.map((option) => (
-        <DialogueOption 
-          key={option.id}
-          option={option}
-        />
-      ))}
+        {optionList && optionList.map((option) => (
+          <DialogueOption 
+            key={option.id}
+            option={option}
+          />
+        ))}
+      </div>
+
+      <div className={styles.subOptions}>
+        {subOptions && subOptions.map((subOption) => (
+          <p key={subOption.id}>{subOption.text}</p>
+        ))}
+      </div>
     </div>
   );
 };
